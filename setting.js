0d04e8b80441248a3dca3e01b4775801c77406cf
ryugameshop9-spec <ryugameshop9@gmail.com>
2025-12-25 13:22:12 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,5 @@
+global.feenya = 150//atur fee nya misal harga asli 1.480 + fee 200 jadi harganya 1.680
+global.apikey = "KhafaTopUp_stscj7bmmvm5s9n0" //isi apikey dari https://khafatopup.my.id minta verifikasi h2h ke admin nya
+
+
+
